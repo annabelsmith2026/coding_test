bf95c60fa2bb698ff1d949fc9a48a36e7d7a9015
annabelsmith2026 <annabelsmith678@outlook.com>
2026-05-02 08:02:22 +0000

    Prepared R package folder and set working directory

--- question_1/question_1_script.R
+++ question_1/question_1_script.R
@@ -1,2 +1,13 @@
 install.packages("devtools")
-create_packages()
+library(devtools)
+
+# Setting working directory
+setwd("/cloud/project")
+
+# Create parent folder structure
+dir.create("question_1/descriptive_stats", recursive = TRUE)
+
+# Create the package
+library(usethis)
+create_package("/cloud/project/question_1/descriptive_stats/descriptiveStats")
+
